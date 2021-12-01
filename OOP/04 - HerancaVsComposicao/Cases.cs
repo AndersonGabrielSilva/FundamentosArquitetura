16394c6bc8b382dd47cc07490f577cd2700cdfe1
Anderson Gabriel Silva <andersongabriel_maria@hotmail.com>
2021-11-30 21:34:21 -0300

    Herança vs Composição

--- OOP/04 - HerancaVsComposicao/Cases.cs
+++ OOP/04 - HerancaVsComposicao/Cases.cs
@@ -2,13 +2,15 @@
 
 namespace OOP
 {
-    #region Caso 1
+     #region Caso 1
 
+    //Caso de Herança
     public class PessoaFisica : Pessoa
     {
         public string Cpf { get; set; }
     }
 
+    //Caso de Composição
     public class PessoaFisica2
     {
         public Pessoa Pessoa { get; set; }
@@ -70,11 +72,15 @@ namespace OOP
         }
     }
 
+    // A desvantagem da herança é que ela traz consigo um alto nivel de acoplamento, neste exemplo abaixo o repositorio tem acesso a todos comportamentos 
+    // da classe herdada, no exemplo a seguir iremos conseguir ver o a vantagem da composição, em exibir apenas o que eu desejo
     public class RepositorioHerancaPessoa : Repositorio<Pessoa>, IRepositorioPessoa
     {
 
     }
 
+    //A AVantagem da composição é que eu consigo expor apenas o que eu realmente quero, neste exemplo eu não quiz exportar o comportamento de excluir
+    //Com a composição é possivel ter um nivel maior de coesão e um nivel menor de acoplamento
     public class RepositorioComposicaoPessoa : IRepositorioPessoa
     {
         private readonly IRepositorio<Pessoa> _repositorioPessoa;
